a3a693bdab4c84b4212626d4c499da32aa5f80bc
foryasith <yasithprashan@gmail.com>
2026-04-03 19:10:34 +0530

    fix: update csproj dependencies for net9 compatibility

--- src/OrderService/Program.cs
+++ src/OrderService/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi;
 using Microsoft.OpenApi.Models;
 using OrderService.Common.Middleware;
 using OrderService.Data;
