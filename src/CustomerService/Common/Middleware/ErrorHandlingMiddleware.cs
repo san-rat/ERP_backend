71353e260a2fc4f0767db0e457b38065b6b4e115
foryasith <yasithprashan@gmail.com>
2026-04-13 12:48:13 +0530

    feat: add global error handling middleware

---
+++ src/CustomerService/Common/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,36 @@
+using System.Net;
+using System.Text.Json;
+
+namespace CustomerService.Common.Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var result = JsonSerializer.Serialize(new
+                {
+                    success = false,
+                    message = ex.Message
+                });
+
+                await context.Response.WriteAsync(result);
+            }
+        }
+    }
+}
